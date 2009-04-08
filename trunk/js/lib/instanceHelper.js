ef9c9ce53cfe76f7844895578f513a8b52f1728d
ndjin.admin <ndjin.admin@c4507336-23a2-11de-8b5c-13accb87f508>
2009-04-08 10:15:26 +0000

    5min tutorial

---
+++ trunk/js/lib/instanceHelper.js
@@ -0,0 +1,57 @@
+var json2Loaded; if (!json2Loaded) {	json2Loaded = true;	document.write('<script type="text/javascript" src="./lib/json2.js"></script>'); }
+var jqueryLoaded; if (!jqueryLoaded) {jqueryLoaded = true; document.write('<script type="text/javascript" src="./lib/jquery/jquery-1.3.2.min.js"></script>');}
+
+
+function InstanceHelper(appName, asyncRequest ){
+    this.ndjinServiceURL = "http://"+ appName + ".ndjin.net/ng/service";
+   	this.async = asyncRequest;
+    
+	this.applyTransition = function ( query )
+	{
+		var result;
+		$.ajax({
+	        url: this.ndjinServiceURL+"/InstanceService/applyTransitionToInstance",
+			async: this.async,
+	        dataType: "jsonp",
+	        data: { requestDataType:'json', responseDataType:'jsonp', data: JSON.stringify( query ) },
+	        success: function (data, textStatus)
+	        {
+				if( data.error )  
+				{
+					alert( data.error.message );
+				}
+				else
+				{
+					result = data.result.instance;
+				}
+			} // success
+		}); // ajax	
+		return result;
+	}
+
+
+	this.getInstances = function ( query )
+	{
+		var result = [];
+		$.ajax({
+	        url: this.ndjinServiceURL+"/InstanceService/getInstances",
+			async: this.async,			
+	        dataType: "jsonp",
+	        data: { requestDataType:'json', responseDataType:'jsonp', data: JSON.stringify( query ) },
+	        success: function (data, textStatus)
+	        {
+				if( data.error )  
+				{
+					alert( data.error.message );
+				}
+				else
+				{
+					result = data.result.instances;
+				}
+			} // success
+		}); // ajax	
+		return result;
+	}
+
+	
+}
